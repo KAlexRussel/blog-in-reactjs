4013e4001f5d3a705bb4c8efda520863b19545e8
KAlexRussel <kouawoualex1234@gmail.com>
2022-11-03 12:57:01 +0100

    setting react
    Signed-off-by: KAlexRussel <kouawoualex1234@gmail.com>

---
+++ client/src/component/footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-1">
+      <div className="foot">
+        <div className="grp">
+          <h3>SHOP</h3>
+          <p>My Account</p>
+          <p>Terms and conditions</p>
+          <p>SReturns</p>
+          <p>FAQs</p>
+        </div>
+        <div className="grp">
+          <h3>COMPANY</h3>
+          <p>About us</p>
+          <p>jobs</p>
+          <p>Services</p>
+          <p>contact</p>
+        </div>
+
+        <div className="grp">
+          <h3>SECTIONS</h3>
+          <p>Design</p>
+          <p>Culture</p>
+          <p>Sneakers</p>
+          <p>Style</p>
+        </div>
+
+        <div className="grp">
+          <h3>MORE</h3>
+          <p>Print Magazine</p>
+          <p>News Letter</p>
+          <p>Archive</p>
+          <p>Style</p>
+        </div>
+      </div>
+      <div className="socials">
+        <span className="css-118najv">
+          <i className="fa-brands fa-linkedin"></i>
+        </span>
+        <span className="css-118najv">
+          <i className="fa-brands fa-facebook"></i>
+        </span>
+        <span className="css-118najv">
+          {" "}
+          <i className="fa-brands fa-youtube"></i>{" "}
+        </span>
+        <span className="css-118najv">
+          <i className="fa-brands fa-twitter"></i>{" "}
+        </span>
+
+        <span className="css-118najv">
+          <i className="fa-brands fa-github"></i>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ client/src/pages/homepage/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "../../component/footer/Footer";
+
+import "./home.css";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <p>
+          To get started with React Router in a web app, you will need a React
+          web app. If you need to create one, we recommend you try Create React
+          App. It’s a popular tool that works really well with React Router.
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
