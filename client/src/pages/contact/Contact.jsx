2380700ae9adc0e37bc29c9ab41ac0c85063d584
KAlexRussel <kouawoualex1234@gmail.com>
2022-11-04 04:42:19 +0100

    cocntact paeg
    Signed-off-by: KAlexRussel <kouawoualex1234@gmail.com>

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,7 +1,31 @@
+import React from "react";
+import Footer from "../../component/footer/Footer";
+import Navbar from "../../component/navbar/Navbar";
+import "./contact.css";
+
 function Contact() {
   return (
     <div>
-      <h1>contact page</h1>
+      <Navbar />
+      <div className="main-con">
+        <div className="parag">
+          <p>
+            If you have a product/brand that you think fits well on my website
+            and want to advertise it, contact me. All of my posts are very well
+            thought out and designed to engage with the reader and always
+            contain a captivating story with carefully planned and executed
+            imagery. If you're interested, ask me and I will send you my Media
+            Kit with more info.
+          </p>
+        </div>
+        <div className="vl"></div>
+        <div className="contact-1">
+          <h3>EMAIL</h3>
+          <p>Kuavo12alex@gmail.com</p>
+        </div>
+      </div>
+      <div></div>
+      <Footer />
     </div>
   );
 }
