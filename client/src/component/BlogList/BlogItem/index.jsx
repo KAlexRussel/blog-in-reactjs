8d0a9c75eebf792d7f202d697e9f491eb2e8e57e
KAlexRussel <kouawoualex1234@gmail.com>
2022-12-06 10:23:17 +0100

    install axios toastify
    Signed-off-by: KAlexRussel <kouawoualex1234@gmail.com>

--- client/src/component/BlogList/BlogItem/index.jsx
+++ client/src/component/BlogList/BlogItem/index.jsx
@@ -16,26 +16,34 @@ const BlogItem = ({
   },
 }) => {
   return (
-    <Link className="blogItem-link" to={`/blog/${id}`}>
-      <div className="blogItem-wrap">
+    <div className="blogItem-wrap">
+      <Link className="blogItem-link" to={`/blog/${id}`}>
         <img className="blogItem-cover" src={cover} alt="cover" />
         <Chip label={category} />
         <h3>{title}</h3>
         <p className="blogItem-desc">{description}</p>
-        <footer>
-          <div className="blogItem-author">
-            <img src={authorAvatar} alt="avatar" />
-            <div>
-              <h6>{authorName}</h6>
-              <p>{createdAt}</p>
-            </div>
+      </Link>
+      <footer>
+        <div className="blogItem-author">
+          <img src={authorAvatar} alt="avatar" />
+          <div>
+            <h6>{authorName}</h6>
+            <p>{createdAt}</p>
           </div>
-          {/* 
+        </div>
+        <div className="operations">
+          <div>
+            <i class="fas fa-pen-square"></i>
+          </div>
+          <div>
+            <i class="fa fa-trash" aria-hidden="true"></i>
+          </div>
+        </div>
+        {/* 
           ➝
         </Link> */}
-        </footer>
-      </div>
-    </Link>
+      </footer>
+    </div>
   );
 };
 
